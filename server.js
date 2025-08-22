d1c836ef7020d3c863583f6799eee7fe2a00b2c2
SameenaAkthar <sameenaaktharm@gmail.com>
2025-08-22 17:25:17 +0530

    commit

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use("/api/users", userRoute);
 app.use("/api/admin", adminRoute); 
